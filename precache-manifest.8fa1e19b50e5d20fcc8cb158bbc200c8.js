62b42497bb4d7427e906a7a1384895ee5e13d7f2
Deynega Andrey <andrtomsk@narod.ru>
2020-01-27 19:16:26 +0700

    Updates

--- precache-manifest.8fa1e19b50e5d20fcc8cb158bbc200c8.js
+++ precache-manifest.8fa1e19b50e5d20fcc8cb158bbc200c8.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3b7f961605ef0b8aec95b20eade06b92",
+    "revision": "d336bc6777ca24696d576934bddb8290",
     "url": "/portfolio-deynega-andrey/index.html"
   },
   {
-    "revision": "1bd5984294a474d47cf7",
+    "revision": "35f0356d1d54be577329",
     "url": "/portfolio-deynega-andrey/static/css/main.e4a5599e.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio-deynega-andrey/static/js/2.a4a5120b.chunk.js"
   },
   {
-    "revision": "1bd5984294a474d47cf7",
-    "url": "/portfolio-deynega-andrey/static/js/main.0c0d7c80.chunk.js"
+    "revision": "35f0356d1d54be577329",
+    "url": "/portfolio-deynega-andrey/static/js/main.9b42fdf2.chunk.js"
   },
   {
     "revision": "df989dbdab12614804dd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-deynega-andrey/precache-manifest.d32ed40c2890dfd367995fd6ff9dd447.js"
+  "/portfolio-deynega-andrey/precache-manifest.8fa1e19b50e5d20fcc8cb158bbc200c8.js"
 );
 
 self.addEventListener('message', (event) => {
