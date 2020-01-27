93700697339e0b6349762330591a88f3b03a458a
Deynega Andrey <andrtomsk@narod.ru>
2020-01-27 19:10:10 +0700

    Updates

---
+++ precache-manifest.d32ed40c2890dfd367995fd6ff9dd447.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b7f961605ef0b8aec95b20eade06b92",
+    "url": "/portfolio-deynega-andrey/index.html"
+  },
+  {
+    "revision": "1bd5984294a474d47cf7",
+    "url": "/portfolio-deynega-andrey/static/css/main.e4a5599e.chunk.css"
+  },
+  {
+    "revision": "596a367adbe768123786",
+    "url": "/portfolio-deynega-andrey/static/js/2.a4a5120b.chunk.js"
+  },
+  {
+    "revision": "1bd5984294a474d47cf7",
+    "url": "/portfolio-deynega-andrey/static/js/main.0c0d7c80.chunk.js"
+  },
+  {
+    "revision": "df989dbdab12614804dd",
+    "url": "/portfolio-deynega-andrey/static/js/runtime~main.87c98ccd.js"
+  }
+]);
